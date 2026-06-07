8b7f70d98d260540c7a2b346fecbd8b6db575f35
Diego Facundo Pérez Nicolau <3607834730101@ingenieria.usac.edu.gt>
2026-06-06 20:02:06 -0600

    [Update] backend Middleware & CORS

--- Practica1/backend/main.py
+++ Practica1/backend/main.py
@@ -3,11 +3,20 @@
 # pip install fastapi uvicorn pyswip
 # uvicorn main:app --reload
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from repositories.prolog_repo import PrologRepo
 from controllers.city_controller import cities_router
 from services.city_service import CityService
 
 app = FastAPI(title="API de Ciudades y Rutas")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite peticiones desde cualquier origen (para desarrollo)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite GET, POST, etc.
+    allow_headers=["*"],
+)
 
 repository = PrologRepo("citys.pl")
 
