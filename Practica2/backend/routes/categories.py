b7fde86d5ed80f3b8519757f47b4e8b80f3a7819
DFaxx <3607834730101@ingenieria.usac.edu.gt>
2026-06-12 16:40:22 -0600

    [Add] Endpoints: Create Update & Delete Categories

--- Practica2/backend/routes/categories.py
+++ Practica2/backend/routes/categories.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from database.mongodb import db
+from bson import ObjectId
 
 categories_bp = Blueprint(
     "categories",
@@ -20,3 +21,57 @@ def get_categories():
         })
 
     return jsonify(categories)
+
+@categories_bp.route("", methods=["POST"])
+def create_category():
+
+    data = request.json
+
+    result = db.categories.insert_one({
+        "name": data["name"]
+    })
+
+    return jsonify({
+        "message": "Categoría creada",
+        "id": str(result.inserted_id)
+    }), 201
+
+
+@categories_bp.route("/<id>", methods=["PUT"])
+def update_category(id):
+
+    data = request.json
+
+    result = db.categories.update_one(
+        {"_id": ObjectId(id)},
+        {
+            "$set": {
+                "name": data["name"]
+            }
+        }
+    )
+
+    if result.matched_count == 0:
+        return jsonify({
+            "message": "Categoría no encontrada"
+        }), 404
+
+    return jsonify({
+        "message": "Categoría actualizada"
+    })
+
+@categories_bp.route("/<id>", methods=["DELETE"])
+def delete_category(id):
+
+    result = db.categories.delete_one({
+        "_id": ObjectId(id)
+    })
+
+    if result.deleted_count == 0:
+        return jsonify({
+            "message": "Categoría no encontrada"
+        }), 404
+
+    return jsonify({
+        "message": "Categoría eliminada"
+    })
