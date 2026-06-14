7f296658801ab4a092fd5828a9ad11ec9cd5d019
DFaxx <3607834730101@ingenieria.usac.edu.gt>
2026-06-13 19:30:30 -0600

    [Add] Start Frontend Login

---
+++ Practica2/frontend/src/pages/Login/index.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import api from "../../services/api";
+
+import "./styles.css";
+
+export default function Login() {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [error, setError] = useState("");
+
+    async function handleSubmit(event) {
+
+        event.preventDefault();
+
+        try {
+
+            const response = await api.post(
+                "/login",
+                {
+                    username,
+                    password
+                }
+            );
+
+            localStorage.setItem(
+                "token",
+                response.data.token
+            );
+
+            alert(response.data.message);
+
+        } catch {
+
+            setError(
+                "Usuario o contraseña incorrectos"
+            );
+        }
+    }
+
+    return (
+        <div className="login-container">
+
+            <div className="login-card">
+
+                <h1>SmartBot</h1>
+
+                <form onSubmit={handleSubmit}>
+
+                    <input
+                        type="text"
+                        placeholder="Usuario"
+                        value={username}
+                        onChange={(e) =>
+                            setUsername(e.target.value)
+                        }
+                    />
+
+                    <input
+                        type="password"
+                        placeholder="Contraseña"
+                        value={password}
+                        onChange={(e) =>
+                            setPassword(e.target.value)
+                        }
+                    />
+
+                    {
+                        error &&
+                        <p className="error">
+                            {error}
+                        </p>
+                    }
+
+                    <button type="submit">
+                        Iniciar Sesión
+                    </button>
+
+                </form>
+
+            </div>
+
+        </div>
+    );
+}
